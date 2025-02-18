ec57c2c4212a6a3f5445091e36be06097322ca92
Furebo <furebodidace582@gmail.com>
2025-02-18 15:15:44 +0200

    solved the port issue

--- app.js
+++ app.js
@@ -59,9 +59,6 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-    console.log("App is listening on port " + PORT);
-});
 
 //connectWithRetry();
 

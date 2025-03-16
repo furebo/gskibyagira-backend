f3e3f3416fd817c36af0a84d4195e482b30f3069
Furebo <furebodidace582@gmail.com>
2025-03-16 12:07:07 +0200

    delete message implemeneted

--- src/routes/MessageRoutes.js
+++ src/routes/MessageRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createMessage,getAllMessages } from '../controllers/messagesController.js';
+import { createMessage,getAllMessages,deleteMessage, editMessage } from '../controllers/messagesController.js';
 
 const router = express.Router();
 router.post('/messages',createMessage);
 router.get('/messages',getAllMessages);
+router.delete('/messages/:id',deleteMessage);
+router.put('/messages/:id',editMessage);
 
 export default router;

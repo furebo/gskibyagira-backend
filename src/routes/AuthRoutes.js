7a7f4a988d768457438a03f85ea444ce3e3bb314
Furebo <furebodidace582@gmail.com>
2025-03-26 16:08:12 +0200

    fixed the isue in authContoller

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { googleLogin } from '../controllers/authController.js';
+import { googleLogin } from '../controllers/authContoller.js';
 const router = express.Router();
 
 router.post('/google-login', googleLogin);

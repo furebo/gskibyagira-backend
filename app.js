da65be446a3585b4b71771f00e8b1e5546909391
Furebo <furebodidace582@gmail.com>
2025-03-29 14:34:35 +0200

    fixed google auth backend side

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ Apply CORS Middleware First
 app.use(
   cors({
-    origin: "https://gskibyagiraburuhukiro.netlify.app", // Allow only this frontend
+    origin: "http://localhost:3000", // Allow only this frontend
     methods: "GET,POST,PUT,DELETE", // Allow necessary HTTP methods
     allowedHeaders: "Content-Type,Authorization", // Allow these headers
   })
@@ -30,7 +30,7 @@ app.use('/api', routes);
 
 // ✅ Database Connection in Production
 const isProduction = process.env.NODE_ENV === 'production';
-if (isProduction) {
+if (isProduction || !isProduction) {
     connectToDatabase()
     .then(() => {
       console.log('Connected to the database');
